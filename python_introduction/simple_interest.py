416284173655e9147a1b08b4c04e97e57e51a0ba
alindafortunate <alindafortunate5@gmail.com>
2024-12-10 11:40:00 +0300

    Added Task 1 about Interest rate

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# A program to calculate the simple interest rate
+# Variabledefinition
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
