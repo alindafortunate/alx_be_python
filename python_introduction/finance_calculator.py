3edb3ce2979a5fb946f427b150f5c25b98106a2a
alindafortunate <alindafortunate5@gmail.com>
2024-12-10 12:33:43 +0300

    Modified the changed total_monthly_expenses to monthly_expenses

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,10 +2,10 @@
 
 # User Input for Financial Details:
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 # Calculate Monthly Savings:
-monthly_savings = float(monthly_income) - float(total_monthly_expenses)
+monthly_savings = monthly_income - monthly_expenses
 
 # Project Annual Savings:
 simple_annual_interest_rate = 0.05
