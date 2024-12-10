81fbbbfc6a6ff280b4dca579f870d9f755502f12
alindafortunate <alindafortunate5@gmail.com>
2024-12-10 12:27:32 +0300

    Modified the output

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
 # A program that calculates finances.
 
 # User Input for Financial Details:
-monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+total_monthly_expenses = float(input("Enter your total monthly expenses: "))
 
 # Calculate Monthly Savings:
 monthly_savings = monthly_income - total_monthly_expenses
@@ -12,4 +12,8 @@ simple_annual_interest_rate = 0.05
 projected_savings = monthly_savings * 12 + (
     monthly_savings * 12 * simple_annual_interest_rate
 )
-print(f"Projected savings after one year, with interest, is: ${projected_savings}")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}")
+# /tmp/correction/3120001615871916859719583418112290743355_5/100739/992906/
+# python_introduction/finance_calculator.py
+# doesn't contain monthly_savings\s*=\s*(monthly_income\s*-\s*monthly_expenses|
+# float\s*\(\s*monthly_income\s*\)\s*-\s*float\s*\(\s*monthly_expenses\s*\))
