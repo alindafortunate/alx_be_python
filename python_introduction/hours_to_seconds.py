53db5cc05ca46fc3650d087a62dd629b4614da46
alindafortunate <alindafortunate5@gmail.com>
2024-12-10 11:58:00 +0300

    Edited formula

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,11 +2,3 @@
 hours = 2
 seconds = hours * 3600
 print(f"{hours} hours(s) is {seconds} seconds.")
-
-
-#  hours_to_seconds.py doesn't
-# contain: ["seconds = hours * 3600"]
-# - hours_to_seconds.py doesn't contain: ["seconds=hours*3600"]
-# - hours_to_seconds.py doesn't contain: ["seconds = 3600 * hours"]
-
-# - hours_to_seconds.py doesn't contain: ["seconds=3600*hours"]
