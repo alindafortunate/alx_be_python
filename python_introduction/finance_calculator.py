f54cb179c191554f1eaca9255bcde4454e66ee83
alindafortunate <alindafortunate5@gmail.com>
2024-12-10 12:37:12 +0300

    Modified the changed total_monthly_expenses to monthly_expenses

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -14,7 +14,3 @@ projected_savings = float(monthly_savings) * 12 + (
 )
 print(f"Your monthly savings are ${monthly_savings}")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}")
-# /tmp/correction/3120001615871916859719583418112290743355_5/100739/992906/
-# python_introduction/finance_calculator.py
-# doesn't contain monthly_savings\s*=\s*(monthly_income\s*-\s*monthly_expenses|
-# float\s*\(\s*monthly_income\s*\)\s*-\s*float\s*\(\s*monthly_expenses\s*\))
