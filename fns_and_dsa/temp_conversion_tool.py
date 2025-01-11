deead00e129d3c69441b4e139b5cf77ad135a606
alindafortunate <alindafortunate5@gmail.com>
2025-01-12 01:20:54 +0300

    third commit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,4 +1,4 @@
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 // 5
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
 
@@ -13,13 +13,20 @@ def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 
-temperature = float(input("Enter the temperature to convert: "))
-question = (
-    input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().capitalize()
-)
-if question == "F":
-    print(f"{temperature}°C is {convert_to_celsius(temperature)}°F")
-elif question == "C":
-    print(f"{temperature}°F is {convert_to_fahrenheit(temperature)}°C")
-else:
-    print("Invalid temperature. Please enter a numeric value.")
+def main():
+    temperature = float(input("Enter the temperature to convert: "))
+    question = (
+        input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+        .strip()
+        .capitalize()
+    )
+    if question == "F":
+        print(f"{temperature}°C is {convert_to_celsius(temperature)}°F")
+    elif question == "C":
+        print(f"{temperature}°F is {convert_to_fahrenheit(temperature)}°C")
+    else:
+        print("Invalid temperature. Please enter a numeric value.")
+
+
+if __name__ == "__main__":
+    main()
