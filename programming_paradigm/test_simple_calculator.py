30fe1c4fc6801dab112aaad7f9488cff78112995
alindafortunate <alindafortunate5@gmail.com>
2025-01-18 16:08:18 +0300

    Initial for exercise 3

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,34 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Setup the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition_method(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertTrue(isinstance(self.calc.add(2, 4), (int, float)))
+
+    def test_subtraction_method(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(-1, -3), 2)
+        self.assertTrue(isinstance(self.calc.subtract(2, 3), (int, float)))
+
+    def test_multiplication_method(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(2, 0), 0)
+
+    def test_division_method(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(4, 2), 2)
+        self.assertRaises(ZeroDivisionError)
+    
+
+if __name__ == "__main__":
+    unittest.main()
