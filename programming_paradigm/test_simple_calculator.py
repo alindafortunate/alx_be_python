39c00a6b3fd5cf06729e45d1b3894cdc99204e79
alindafortunate <alindafortunate5@gmail.com>
2025-01-18 16:11:00 +0300

    test method names

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,24 +7,24 @@ class TestSimpleCalculator(unittest.TestCase):
         """Setup the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_addition_method(self):
+    def test_addition(self):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertTrue(isinstance(self.calc.add(2, 4), (int, float)))
 
-    def test_subtraction_method(self):
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(3, 2), 1)
         self.assertEqual(self.calc.subtract(-1, -3), 2)
         self.assertTrue(isinstance(self.calc.subtract(2, 3), (int, float)))
 
-    def test_multiplication_method(self):
+    def test_multiplication(self):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(2, 0), 0)
 
-    def test_division_method(self):
+    def test_division(self):
         """Test the division method."""
         self.assertEqual(self.calc.divide(4, 2), 2)
         self.assertRaises(ZeroDivisionError)
